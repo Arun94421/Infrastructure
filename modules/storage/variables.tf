892d468277f6bd8a3e6dd2443abafe08a35b0ce2
Arun <arunadhithya10@gmail.com>
2025-11-12 17:15:41 +0530

    Add files via upload

---
+++ modules/storage/variables.tf
@@ -0,0 +1,37 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account (must be globally unique)"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Name of the blob container"
+  type        = string
+}
+
+variable "account_tier" {
+  description = "Storage account tier"
+  type        = string
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Storage account replication type"
+  type        = string
+  default     = "LRS"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
