57e3045b72e1fbd914785ad3cb8e54565ff44a85
Arun <arunadhithya10@gmail.com>
2025-11-12 17:14:01 +0530

    Create storage.tf

---
+++ modules/storage/storage.tf
@@ -0,0 +1,15 @@
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
